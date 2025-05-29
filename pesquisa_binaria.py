8bf24eda9aceabeeeed4fa1e1bc54b7078c20a3e
joão.pedro_git <jpbrasil763@gmail.com>
2025-05-28 23:29:06 -0300

    Melhoria da pesquisa

--- pesquisa_binaria.py
+++ pesquisa_binaria.py
@@ -1,22 +1,24 @@
 import pandas as pd
-dataframe_das_criptomoedas = pd.read_csv('cripto_em_ordem_alfabetica.csv')
 
+dataframe_das_criptomoedas = pd.read_csv('cripto_em_ordem_alfabetica.csv')
 df_copia = dataframe_das_criptomoedas
 
-cripto = 'w'.lower()
+pesquisa_usuario = 'i'.lower()
 
-cont = 0
 
-llll= list()
-t = str()
-lista= list()
 
-def pesquisando (dataframe, nome_da_cripto):
+lista_cordenadas_criptomoedas_encontradas= []
+
+lista_criptomoedas_encontradas = []
+
+
+def pesquisador (dataframe, nome_da_cripto):
+
     baixo = 0
-    alto = len (dataframe_das_criptomoedas)  - 1
+    alto = len (dataframe)  - 1
     while baixo <= alto:
         meio = (baixo + alto) // 2
-        while meio in llll:
+        while meio in lista_cordenadas_criptomoedas_encontradas:
             meio += 1 
             
         if meio > alto:
@@ -36,14 +38,24 @@ def pesquisando (dataframe, nome_da_cripto):
         
     return None
 
-cont = 0
 
-while True:    
-    t = pesquisando(df_copia, cripto)
-    if t == None:
-        break
-    llll.append(t[1])
-    lista.append(t[0])
-    df_copia.drop(t[1], axis=0, inplace=True)
-    print (lista)
-    cont+= 1
+
+def pesquisa_de_criptomoedas(dataframe, cripto):
+    while True:    
+        
+        resultado = pesquisador (dataframe, cripto)
+        
+        if resultado == None:
+            break
+        
+        lista_cordenadas_criptomoedas_encontradas.append(resultado[1])
+        
+        lista_criptomoedas_encontradas.append(resultado[0])
+
+        dataframe.drop(resultado[1], axis=0, inplace=True)
+    return lista_criptomoedas_encontradas
+
+
+resultado_pesquisa = pesquisa_de_criptomoedas(df_copia, pesquisa_usuario )
+
+print (resultado_pesquisa)
